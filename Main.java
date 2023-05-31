fd3356965d97942d79533c85518a7a0897b6e353
michal169751 <127328268+michal169751@users.noreply.github.com>
2023-05-31 14:40:07 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,83 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+class WrongStudentName extends Exception { }
+class WrongAge extends Exception { }
+
+class Main {
+    public static Scanner scan = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        while(true) {
+            try {
+                int ex = menu();
+                switch(ex) {
+                    case 1: exercise1(); break;
+                    case 2: exercise2(); break;
+                    case 3: exercise3(); break;
+                    default: return;
+                }
+            } catch(IOException e) {
+
+            } catch(WrongStudentName e) {
+                System.out.println("Błędne imie studenta!");
+            } catch(WrongAge e) {
+                System.out.println("Błędny wiek!");
+            }
+        }
+    }
+
+    public static int menu() {
+        System.out.println("Wciśnij:");
+        System.out.println("1 - aby dodać studenta");
+        System.out.println("2 - aby wypisać wszystkich studentów");
+        System.out.println("3 - aby wyszukać studenta po imieniu");
+        System.out.println("0 - aby wyjść z programu");
+        return scan.nextInt();
+    }
+    public static int ReadAge() throws WrongAge {
+      scan.nextLine();
+      System.out.println("Podaj wiek: ");
+      var age = scan.nextInt();
+      if(age < 1 || age > 100)
+        throw new WrongAge();
+      return age;
+    }
+    public static String ReadName() throws WrongStudentName {
+        System.out.println("Podaj imie: ");
+        String name = scan.nextLine();
+        if(name.contains(" "))
+            throw new WrongStudentName();
+
+        return name;
+    }
+
+    public static void exercise1() throws IOException, WrongStudentName, WrongAge {
+        var name = ReadName();
+        var age = ReadAge();
+        scan.nextLine();
+        System.out.println("Podaj datę urodzenia DD-MM-YYY");
+        var date = scan.nextLine();
+        (new Service()).addStudent(new Student(name, age, date));
+    }
+
+    public static void exercise2() throws IOException {
+        var students = (new Service()).getStudents();
+        for(Student current : students) {
+            System.out.println(current.ToString());
+        }
+    }
+
+    public static void exercise3() throws IOException {
+        scan.nextLine();
+        System.out.println("Podaj imie: ");
+        var name = scan.nextLine();
+        var wanted = (new Service()).findStudentByName(name);
+        if(wanted == null)
+            System.out.println("Nie znaleziono...");
+        else {
+            System.out.println("Znaleziono: ");
+            System.out.println(wanted.ToString());
+        }
+    }
+}
