d8b002166c9621d4fff0829b79908e6cef683469
michal169751 <127328268+michal169751@users.noreply.github.com>
2023-06-06 09:14:09 +0000

    6_2

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 
 class WrongStudentName extends Exception { }
 class WrongAge extends Exception { }
+class WrongNumber extends Exception { }
 
 class Main {
     public static Scanner scan = new Scanner(System.in);
@@ -23,17 +24,35 @@ class Main {
                 System.out.println("Błędne imie studenta!");
             } catch(WrongAge e) {
                 System.out.println("Błędny wiek!");
+            } catch(WrongNumber e) {
+                System.out.println("Błędny numer!");
             }
         }
     }
 
-    public static int menu() {
+    public static int menu() throws WrongNumber {
+        Scanner scan = new Scanner(System.in);
         System.out.println("Wciśnij:");
         System.out.println("1 - aby dodać studenta");
         System.out.println("2 - aby wypisać wszystkich studentów");
         System.out.println("3 - aby wyszukać studenta po imieniu");
         System.out.println("0 - aby wyjść z programu");
-        return scan.nextInt();
+        String input = scan.nextLine();
+        if (input.length() != 1) {
+          throw new WrongNumber();
+        }
+        char menu = input.charAt(0);
+        int ascii = (int) menu;
+        if (ascii >= 48 && ascii <= 57) {
+          int z = Character.getNumericValue(menu);
+          if (z > 3 || z < 0) {
+            throw new WrongNumber();
+          }
+          return z;
+        }
+      else {
+        throw new WrongNumber();
+      }
     }
     public static String ReadName() throws WrongStudentName {
         System.out.println("Podaj imie: ");
@@ -55,12 +74,12 @@ class Main {
     public static void exercise1() throws IOException, WrongStudentName, WrongAge {
         var name = ReadName();
         var age = ReadAge();
+      
         scan.nextLine();
         System.out.println("Podaj datę urodzenia DD-MM-YYY");
         var date = scan.nextLine();
         (new Service()).addStudent(new Student(name, age, date));
     }
-
     public static void exercise2() throws IOException {
         var students = (new Service()).getStudents();
         for(Student current : students) {
